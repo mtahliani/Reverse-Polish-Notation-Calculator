f74ee1d73eae9d470740e4665ffe9e3a555f6d4b
milantahliani <milanmt@bu.edu>
2021-09-27 17:07:57 -0400

    Add PS1 Files

---
+++ PS1/cmake-build-debug/CMakeFiles/PS1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PS1.dir/main.c.o"
+  "PS1"
+  "PS1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PS1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
