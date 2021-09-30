09df46a521ecc2f1202e91e6f513ba8202ab86d5
milantahliani <milanmt@bu.edu>
2021-09-30 00:42:55 -0400

    test

--- PS1 Real /calculatorheader.c
+++ PS1 Real /calculatorheader.c
@@ -5,25 +5,25 @@
 #include <stdio.h>
 #include "calculatorheader.h"
 
-float addition(float val1, float val2){
+float addition(float val1, float val2) {
     float sum = val1 + val2;
 
     return sum;
 }
 
-float multiplication(float val1, float val2){
+float multiplication(float val1, float val2) {
     float product = val1 * val2;
 
     return product;
 }
 
-float subtraction(float val1, float val2){
+float subtraction(float val1, float val2) {
     float difference = val1 - val2;
 
     return difference;
 }
 
-float division(float val1, float val2){
+float division(float val1, float val2) {
 
     float quotient = val1 / val2;
 
@@ -36,19 +36,19 @@ float division(float val1, float val2){
 
 }
 
-float power(float val1, float val2){
+float power(float val1, float val2) {
     float temp, ans;
     float res = 1.0;
-    if (val2 < 0){
+    if (val2 < 0) {
         return -1;
     }
-    if (val2 == 0){
+    if (val2 == 0) {
         return 1;
     }
-    if (val2 == 1){
+    if (val2 == 1) {
         return val1;
     }
-    while (val2 > 0){
+    while (val2 > 0) {
         res *= val1;
         val2--;
     }
@@ -57,28 +57,28 @@ float power(float val1, float val2){
 
 }
 
-float recursivePower(float val1, float val2){
+float recursivePower(float val1, float val2) {
 
-        float res = 1;
-        float base = val1;
-        float exp = val2;
-        if (val2 == 0){
-            return res;
-        }
-
-        if (val2 == 1){
-            return val1;
-        }
-
-        if (val2 < 0) {
-            return -1;
-        }
-
-        if (val2 > 1) {
-            return (val1 * recursivePower(base, exp-res));
-        }
-
+    float res = 1;
+    float base = val1;
+    float exp = val2;
+    if (val2 == 0) {
         return res;
+    }
+
+    if (val2 == 1) {
+        return val1;
+    }
+
+    if (val2 < 0) {
+        return -1;
+    }
+
+    if (val2 > 1) {
+        return (val1 * recursivePower(base, exp - res));
+    }
+
+    return res;
 
 }
 
---
+++ PS1/cmake-build-debug/math.h
@@ -0,0 +1,8 @@
+//
+// Created by Milan Tahliani on 9/30/21.
+//
+
+#ifndef PS1_MATH_H
+#define PS1_MATH_H
+
+#endif //PS1_MATH_H
