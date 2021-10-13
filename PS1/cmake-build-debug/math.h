70adb3186818bef6bc562cc2a3d59509a70e4f88
milantahliani <milanmt@bu.edu>
2021-10-13 13:56:30 -0400

    Initial PS2 Commit

--- PS1/cmake-build-debug/math.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by Milan Tahliani on 9/30/21.
-//
-
-#ifndef PS1_MATH_H
-#define PS1_MATH_H
-
-#endif //PS1_MATH_H
---
+++ untitled/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main() {
+    short foo = 85476565464;
+    printf(foo);
+    return 0;
+}
