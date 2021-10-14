4adcccea769e2cf69bbd37b409b15486755152dd
milantahliani <milanmt@bu.edu>
2021-10-14 13:48:34 -0400

    Problems 1 and 2 done

--- PS2/main.c
+++ PS2/main.c
@@ -2,10 +2,33 @@
 #include "ps2.h"
 
 int main() {
-    int test[] ={72,90,100,36,21,15,76,-6,63,41,99,27,3,66,19,16,27,47,0,23};
-    int length = sizeof test / sizeof *test;
-    int *largest, smallest;
-    printf(test, length, largest, smallest);
-    printf("test for commit again");
+    //running large and small function
+    const int test[] ={72,90,100,36,21,15,76,-6,63,41,99,27,3,66,19,16,27,47,0,23};
+    int arrayLength = sizeof test / sizeof(int);
+    int largest;
+    int smallest;
+    large_and_small(test, arrayLength, &largest, &smallest);
+    printf("%i\n", largest);
+    printf("%i\n", smallest);
+
+    //running inner product function
+    const double left[] = {15.26, 28.20, 6.71, 72.22, 92.85, 15.50, 85.64, 57.41, 44.40, 72.91};
+    const double right[] = {9.17, 4.8, 10.9, 10.12, 17.18, 3.9, 6.19, 5.4, 3.13, 6.1};
+    int leftArrayLength = sizeof left / sizeof(double);
+    int rightArrayLength = sizeof right / sizeof(double);
+
+    if (leftArrayLength != rightArrayLength){
+        printf("Re-enter the arrays to be arrays of equal length");
+    }
+    double res = inner_product(left, right, leftArrayLength);
+    printf("%f", res);
+
+    //running the same or not same function
+
+
+
+
+
+
     return 0;
 }
--- PS2/ps2.c
+++ PS2/ps2.c
@@ -4,18 +4,37 @@
 
 #include "ps2.h"
 
-int large_and_small(const int* array, int length, int* largest, int* smallest){
-    length = sizeof array / sizeof *array;
-    int p = array[5];
-    return length;
+void large_and_small(const int* array, int length, int* largest, int* smallest){
+    *largest = *array;
+    *smallest = *array;
+    //initialize the smallest and the largest values to be first val in array
 
+    for (int i = 0; i < length; i++) {
 
+        if (*largest < *array) {
+            *largest = *array;
+            //if there is a larger value set largest to that
+        }
+
+        if (*smallest > *array){
+            *smallest = *array;
+            //if there is a smaller value set smallest to that
+
+        }
+        *array++;
+        //increment comparable position
+    }
 }
 
 
 double inner_product(const double* leftArray, const double* right_array, int length){
-    double res;
-    return res;
+    double sumTotal = 0;
+    //initialize variable to be returned as the sum total
+    for (int i = 0; i < length; i++){
+        sumTotal += (*leftArray++ * *right_array++);
+        //compute and increment the product of the position
+    }
+    return sumTotal;
 
 }
 
